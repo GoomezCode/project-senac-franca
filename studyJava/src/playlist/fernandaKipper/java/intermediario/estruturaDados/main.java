441da234b9e2e091d6f3c11c5ba4bd1e9040a236
goomez <danielgdpaula@gmail.com>
2026-04-18 00:16:56 -0300

    .

--- studyJava/src/playlist/fernandaKipper/java/intermediario/estruturaDados/main.java
+++ studyJava/src/playlist/fernandaKipper/java/intermediario/estruturaDados/main.java
@@ -1,4 +1,10 @@
 package playlist.fernandaKipper.java.intermediario.estruturaDados;
 
+import playlist.fernandaKipper.java.intermediario.estruturaDados.carro.Carro;
+import playlist.fernandaKipper.java.intermediario.estruturaDados.pessoa.pessoa;
+
 public class main {
+    static void main(String[] args) {
+        pessoa pessoa = new pessoa(new Carro());
+    }
 }
--- studyJava/src/playlist/fernandaKipper/java/intermediario/estruturaDados/pessoa/pessoa.java
+++ studyJava/src/playlist/fernandaKipper/java/intermediario/estruturaDados/pessoa/pessoa.java
@@ -1,4 +1,12 @@
 package playlist.fernandaKipper.java.intermediario.estruturaDados.pessoa;
 
+import playlist.fernandaKipper.java.intermediario.estruturaDados.carro.Carro;
+
 public class pessoa {
+    Carro carro;
+
+    public pessoa(Carro carro){
+        this.carro = carro;
+
+    }
 }
