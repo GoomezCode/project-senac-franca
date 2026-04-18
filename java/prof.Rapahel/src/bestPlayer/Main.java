665ba53f2b11c0727622412b5f400842ed6481d2
goomez <danielgdpaula@gmail.com>
2026-04-18 12:06:16 -0300

    Task: bestPlayer in java Complete and 100% functional

--- java/prof.Rapahel/src/bestPlayer/Main.java
+++ java/prof.Rapahel/src/bestPlayer/Main.java
@@ -2,18 +2,17 @@ package bestPlayer;
 
 
 import javax.management.InvalidAttributeValueException;
+import java.text.DecimalFormat;
 import java.util.*;
 
 public class Main {
+	static DecimalFormat df = new DecimalFormat("0.00");
+	static List<Map> players = new ArrayList<>();
+	static int totalVotos = 0;
 	public static void main(String[] args) {
-		List<Map> players = new ArrayList<>();
-
-
-
-
 		Scanner input = new Scanner(System.in);
-
 		servicePlayer.clear();
+
 		while(true){
 			try{
 				Map<String,Integer> player = new HashMap<>();
@@ -24,13 +23,19 @@ public class Main {
 				if (opc == 0){
 					servicePlayer.clear();
 					System.out.println("");
+					players = servicePlayer.setPorcentagem(players, totalVotos);
+					windowStatus();
+					System.out.println("");
 					System.out.println("Sistema finalizado...");
 					break;
 				}
 				if(opc >23 || opc < 0){throw new InvalidAttributeValueException("Camisa não encontrada!");}
+				boolean f = false;
+				servicePlayer.addVotos(players, player, opc);
+
 				servicePlayer.clear();
 				System.out.printf("Camisa %1$d votado!!\n\n", opc);
-
+				totalVotos+=1;
 			} catch(InvalidAttributeValueException e){
 				servicePlayer.clear();
 				System.out.println("Erro: "+e.getMessage());
@@ -42,9 +47,26 @@ public class Main {
 				input.nextLine();
 			}
 		}
+	}
 
+	public static void windowStatus(){
+		List<Double> porcentual = new ArrayList<>();
+		for (int i = 0; i<players.size(); i++){
+			double camisa = (double) players.get(i).get("camisa");
+			double votos = (double) players.get(i).get("voto");
+			double porcentagem = (double) players.get(i).get("porcentagem");
+			porcentual.add(porcentagem);
+//			System.out.printf("Camisa: %1$.2f | Votos: %2$.2f | Porcentual: %3$.2f\n");
 
-
-
+			System.out.println("Camisa: "+(int)camisa+" | Votos: "+(int)votos+ " | Porcentagem: "+df.format(porcentagem)+"%");
+		}
+		System.out.println("");
+		double maxPorcentual = Collections.max(porcentual);
+		for(Map v: players){
+			if(v.containsValue(maxPorcentual)){
+				System.out.println("Melhor jogador --> Camisa: "+v.get("camisa")+", Votos: "+v.get("voto")+", Porcentual: "+df.format(v.get("porcentagem"))+"%");
+			}
+		}
+		System.out.println("\nTotal votos: "+totalVotos);
 	}
 }
--- java/prof.Rapahel/src/bestPlayer/servicePlayer.java
+++ java/prof.Rapahel/src/bestPlayer/servicePlayer.java
@@ -1,5 +1,10 @@
 package bestPlayer;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class servicePlayer {
     public static void clear(){
         try{
@@ -14,6 +19,36 @@ public class servicePlayer {
         }
     }
 
+    public static void addVotos(List<Map> players, Map<String, Integer> player, int opc){
+        boolean f = false;
+        for (int i =0; i < players.size(); i++){
+            if (players.get(i).containsValue(opc)){
+                if (opc == (int) players.get(i).get("camisa")){
+                    int voto = (int) players.get(i).get("voto");
+                    players.get(i).replace("voto", voto, voto+1);
+                    f = true;
+                }
+            }
+        }if (f == false){
+            player.put("camisa", opc);
+            player.put("voto", 1);
+            players.add(player);
+        }
+    }
 
+    public static List<Map> setPorcentagem(List<Map> players, int totalVotos){
+        List<Map> newPlayers =  new ArrayList<Map>();
+        for (int i =0; i < players.size(); i++){
+            Map<String, Double> player =  new HashMap<>();
 
+            int camisa  = (int) players.get(i).get("camisa");
+            int voto  = (int) players.get(i).get("voto");
+
+            player.put("camisa", (double) camisa);
+            player.put("voto",  (double) voto);
+            player.put("porcentagem", (double) (voto *100)/ totalVotos);
+            newPlayers.add(player);
+        }
+        return newPlayers;
+    }
 }
