b9b11904af1f9540290f125988b37567e8a65698
goomez <danielgdpaula@gmail.com>
2026-05-04 04:47:16 -0300

    testing java swing with spring boot

---
+++ java/testUi/src/main/java/org/goomez/testui/model/userModel.java
@@ -0,0 +1,15 @@
+package org.goomez.testui.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class userModel {
+    private int id;
+    private String name;
+    private String nascimento;
+    private String email;
+}
---
+++ java/testUi/src/main/java/org/goomez/testui/service/userService.java
@@ -0,0 +1,8 @@
+package org.goomez.testui.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class userService {
+
+}
