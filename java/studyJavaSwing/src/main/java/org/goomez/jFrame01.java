3d1fbf2a0ddb0520767a572828c4fa73fcd46340
goomez <danielgdpaula@gmail.com>
2026-05-06 20:32:38 -0300

    .

--- java/studyJavaSwing/src/main/java/org/goomez/jFrame01.java
+++ java/studyJavaSwing/src/main/java/org/goomez/jFrame01.java
@@ -3,17 +3,16 @@ package org.goomez;
 import javax.swing.*;
 import java.awt.*;
 
-public class jFrame01 {
-    static void main() {
-        JFrame frame = new JFrame(); // create frame
-        frame.setTitle("Primary frame"); // set title of frame
-        frame.setSize(400, 400); // set the x and y dimension
-        frame.setResizable(false); // prevent frame from being resized
-        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE); // exit out of application
-        frame.setVisible(true); // make frame visible
+public class jFrame01 extends JFrame {
+    jFrame01() {
+        this.setTitle("Primary frame"); // set title of frame
+        this.setSize(400, 400); // set the x and y dimension
+        //this.setResizable(false); // prevent frame from being resized
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE); // exit out of application
+        // this.setVisible(true); // make frame visible
 
         ImageIcon image = new ImageIcon("Local da imagem"); // create an ImageIcon
-        frame.setIconImage(image.getImage()); // change icon of frame
-        frame.getContentPane().setBackground(new Color(255, 255, 255)); // change color of background
+        this.setIconImage(image.getImage()); // change icon of frame
+        this.getContentPane().setBackground(new Color(255, 255, 255)); // change color of background
     }
 }
